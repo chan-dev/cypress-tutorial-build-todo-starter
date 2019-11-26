ad36b4d4067a77dd42aaf2dd63f006f5d28bce83
chan-dev <christian.m.austria@gmail.com>
2019-11-26 19:43:36 +0800

    Add additional check to test if a todo contains the right text

--- cypress/integration/tutorial/app-init.spec.js
+++ cypress/integration/tutorial/app-init.spec.js
@@ -2,7 +2,13 @@ describe('App initialization', () => {
   it('Loading todos on page load', () => {
     cy.seedAndVisit();
     cy.fixture('todos').then(todos => {
-      cy.get('.todo-list li').should('have.length', todos.length);
+      cy.get('.todo-list li').as('todoList');
+
+      cy.get('@todoList').each(($el, i) => {
+        cy.wrap($el).should('contain.text', todos[i].name);
+      });
+
+      cy.get('@todoList').should('have.length', todos.length);
     });
   });
 
